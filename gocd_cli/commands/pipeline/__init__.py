ed30c36c37f10e745a5664632ba04f89a43e5f0a
Björn Andersson <ba@sanitarium.se>
2015-08-05 22:21:22 +0800

    Add Unlock pipeline command

--- gocd_cli/commands/pipeline/__init__.py
+++ gocd_cli/commands/pipeline/__init__.py
@@ -2,7 +2,7 @@ from gocd_cli.command import BaseCommand
 
 from .retrigger_failed import RetriggerFailed
 
-__all__ = ['RetriggerFailed', 'Trigger']
+__all__ = ['RetriggerFailed', 'Trigger', 'Unlock']
 
 
 class Trigger(BaseCommand):
@@ -13,3 +13,19 @@ class Trigger(BaseCommand):
 
     def run(self):
         return self.pipeline.schedule()
+
+
+class Unlock(BaseCommand):
+    usage = ' '
+    usage_summary = 'Unlocks the named pipeline if it\'s currently locked'
+
+    def __init__(self, server, name):
+        self.pipeline = server.pipeline(name)
+
+    def run(self):
+        response = self.pipeline.status()
+
+        if response and response['locked']:
+            return self.pipeline.unlock()
+        else:
+            return False
--- tests/commands/test_pipeline.py
+++ tests/commands/test_pipeline.py
@@ -3,7 +3,7 @@ from mock import MagicMock
 
 from gocd import Server
 from gocd.api import Pipeline
-from gocd_cli.commands.pipeline import Trigger
+from gocd_cli.commands.pipeline import Trigger, Unlock
 
 
 @pytest.fixture
@@ -14,15 +14,33 @@ def go_server():
     return server
 
 
-@pytest.fixture
-def trigger_command(go_server):
-    return Trigger(go_server, 'Simple-Pipeline')
-
-
 class TestTrigger(object):
     def test_triggers_the_named_pipeline(self, go_server):
-        t = Trigger(go_server, 'Simple-Pipeline')
-        t.run()
+        cmd = Trigger(go_server, 'Simple-Pipeline')
+        cmd.run()
 
         go_server.pipeline.assert_called_once_with('Simple-Pipeline')
-        t.pipeline.schedule.assert_called_once_with()
+        cmd.pipeline.schedule.assert_called_once_with()
+
+
+class TestUnlock(object):
+    @pytest.fixture(autouse=True)
+    def setup(self, go_server):
+        self.cmd = Unlock(go_server, 'Simple-Pipeline')
+
+    def test_checks_if_pipeline_is_unlocked(self):
+        self.cmd.run()
+
+        self.cmd.pipeline.status.assert_called_once_with()
+
+    def test_doesnt_unlock_if_pipeline_is_already_unlocked(self):
+        self.cmd.pipeline.status.return_value = dict(locked=False)
+        self.cmd.run()
+
+        assert not self.cmd.pipeline.unlock.called
+
+    def test_unlocks_if_pipeline_is_locked(self):
+        self.cmd.pipeline.status.return_value = dict(locked=True)
+        self.cmd.run()
+
+        self.cmd.pipeline.unlock.assert_called_with()
